9c798bef5d5114feddbbd07685e5b5dc7c7bd711
albert <alber@gmail.com>
2016-11-24 11:01:14 +0800

    add progress count

--- app/src/main/java/com/example/user/yvd112402/MainActivity.java
+++ app/src/main/java/com/example/user/yvd112402/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -20,12 +21,14 @@ public class MainActivity extends AppCompatActivity {
     private InputStream inputStream = null;
     private ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
     ImageView img;
+    TextView tv;
+    double sum = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         img = (ImageView) findViewById(R.id.imageView);
-
+        tv = (TextView) findViewById(R.id.textView);
         new Thread() {
             @Override
             public void run() {
@@ -42,13 +45,19 @@ public class MainActivity extends AppCompatActivity {
                     byte[] buffer = new byte[64]; // buffer ( 每次讀取長度 )
                     int readSize = 0; // 當下讀取長度
                     int readAllSize = 0;
-                    double sum = 0;
 
                     while ((readSize = inputStream.read(buffer)) != -1)
                     {
                         outputStream.write(buffer, 0, readSize);
                         readAllSize += readSize;
-                        sum = (readAllSize / fullSize) * 100; // 累計讀取進度
+                        sum = (readAllSize / fullSize) * 100; // 累計讀取進度\
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                tv.setText(String.valueOf(sum));
+                            }
+                        });
+
                     }
                 } catch (ProtocolException e) {
                     e.printStackTrace();
